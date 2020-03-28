49266eebe9b44abfd2599ab1e46e66f14329a5b9
Florian Laschober <flo.laschober@gmail.com>
2020-03-28 13:49:28 +0100

    Updated configuration for LED Manager Added brighness support

--- uC/include/Configuration.h
+++ uC/include/Configuration.h
@@ -1,22 +1,42 @@
 #ifndef _CONFIGURATION_H_
 #define _CONFIGURATION_H_
 
+/*************************
+ * LED Configuration:
+ *************************/
 
-//Defaults
+//LED Data pin
+#define LED_DATA_PIN			D8
+//LED Type
+#define LED_TYPE                WS2812B
+//LED Color mode
+#define LED_COLOR_MODE          GRB
+
+/*************************
+ * Default values:
+ *************************/
 #define DEFAULT_NUM_LED			5
 #define DEFAULT_NUM_FRAMES		2
 #define DEFAULT_ANIM_ENABLE		true
 #define DEFAULT_BRIGHTNESS		255
 #define DEFAULT_FRAMERATE		5
 
-//LED Data pin for WSWS2812B LEDs
-#define DATA_PIN				D8
+/*************************
+ * WIFI configuration:
+ *************************/
 
-#define HTTP_REST_PORT			80
-#define WIFI_RETRY_DELAY		500
-#define MAX_WIFI_INIT_RETRY		50
 #define WIFI_SSID				"Quellengasse"
 #define WIFI_PASSWORD			"ADBE1788###"
+
+//can be changed to improve startup speed of the programm
+#define WIFI_RETRY_DELAY		500
+#define MAX_WIFI_INIT_RETRY		50
+//should not be changed unless you have a good reason to do so
+#define HTTP_REST_PORT			80
+
+/*************************
+ * Display configuration:
+ *************************/
 
 
 #endif
